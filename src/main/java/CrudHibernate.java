9b43d08f40f1314d41632ff8ef7a99053095afeb
cristian <cregueiromartinez@danielcastelao.org>
2024-11-17 13:03:21 +0100

    documentacion de crud hibernate

--- src/main/java/CrudHibernate.java
+++ src/main/java/CrudHibernate.java
@@ -7,9 +7,29 @@ import jakarta.persistence.Persistence;
 import java.math.BigDecimal;
 import java.util.List;
 
+/**
+ * Clase con los metodos crud de hibernate
+ * @author cristian
+ * @version 1.0
+ */
 public class CrudHibernate {
 
+    /**
+     * Metodo para insertar valores en la base de datos con hibernate
+     * @param personasList la lista de personas
+     */
     public void insertValuesPersonas(List<Persona> personasList){
+        /*
+          Explicacion:
+          1. Creamos los objetos necesarios para operar con hibernate
+          2. iniciamos la transaccion
+          3. metemos el codigo dentro de un try-cacth
+          4. tx.begin(); -> Significa que podemos empezar a mandar las consultas pero no se enviarán hasta poner el commit
+          5. llamamos al metodo persists para insertar los resultados a la base de datos
+          6. cerramos con .commit()
+          7. finalmente preguntamos si la transaccion sigue activa o hubo algun problema
+          8. si es así, hacemos un rollback para que no se envía nada
+         */
         EntityManagerFactory managerFactory = Persistence.createEntityManagerFactory("default");
         EntityManager em = managerFactory.createEntityManager();
         EntityTransaction tx = em.getTransaction();
@@ -28,7 +48,22 @@ public class CrudHibernate {
         }
     }
 
+    /**
+     * Metodo para actualizar valores en la base de datos con hibernate
+     * @param metodos el metodo que devuelve una Persona
+     */
     public void updateValuesPersonas(Metodos metodos){
+        /*
+          Explicacion:
+          1. Creamos los objetos necesarios para operar con hibernate
+          2. iniciamos la transaccion
+          3. metemos el codigo dentro de un try-cacth
+          4. tx.begin(); -> Significa que podemos empezar a mandar las consultas pero no se enviarán hasta poner el commit
+          5. llamamos al metodo merge para actualizar los resultados de la base de datos
+          6. cerramos con .commit()
+          7. finalmente preguntamos si la transaccion sigue activa o hubo algun problema
+          8. si es así, hacemos un rollback para que no se envía nada
+         */
         EntityManagerFactory managerFactory = Persistence.createEntityManagerFactory("default");
         EntityManager em = managerFactory.createEntityManager();
         EntityTransaction tx = em.getTransaction();
@@ -44,7 +79,22 @@ public class CrudHibernate {
         }
     }
 
+    /**
+     * Metodo para eliminar valores en la base de datos con hibernate
+     * @param id clave primaria del elemento a eliminar
+     */
     public void deletePersona(BigDecimal id) {
+        /*
+          Explicacion:
+          1. Creamos los objetos necesarios para operar con hibernate
+          2. iniciamos la transaccion
+          3. metemos el codigo dentro de un try-cacth
+          4. tx.begin(); -> Significa que podemos empezar a mandar las consultas pero no se enviarán hasta poner el commit
+          5. llamamos al metodo remove para eliminar los resultados de la base de datos
+          6. cerramos con .commit()
+          7. finalmente preguntamos si la transaccion sigue activa o hubo algun problema
+          8. si es así, hacemos un rollback para que no se envía nada
+         */
         EntityManagerFactory managerFactory = Persistence.createEntityManagerFactory("default");
         EntityManager em = managerFactory.createEntityManager();
         EntityTransaction tx = em.getTransaction();
