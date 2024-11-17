d27529b4617e6c636837a59c8571c912d6a2fc16
cristian <cregueiromartinez@danielcastelao.org>
2024-11-17 12:57:57 +0100

    documentacion de la entidad

--- src/main/java/entity/Persona.java
+++ src/main/java/entity/Persona.java
@@ -5,14 +5,27 @@ import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 
+/**
+ * Clase persona que será la entidad en la base de datos
+ * @author cristian
+ * @version 1.0
+ */
 @Entity
 @Table(name = "personas", schema = "public")
 public class Persona {
 
+    /**
+     * El id simboliza la clave primaria que no puede ser null
+     * el colum simboliza el nombre de la columna de la tabla
+     */
     @Id
     @Column(name = "id", nullable = false)
     private BigDecimal id;
 
+    /**
+     * El size simboliza el tamaño de la columna maximo
+     * el colum simboliza el nombre de la columna de la tabla
+     */
     @Size(max = 32)
     @Column(name = "nombre", length = 32)
     private String nombre;
@@ -24,6 +37,10 @@ public class Persona {
     @Column(name = "salario")
     private BigDecimal salario;
 
+    /**
+     * getter y setter de la entidad
+     * @return el valor correspondiente
+     */
     public BigDecimal getId() {
         return id;
     }
