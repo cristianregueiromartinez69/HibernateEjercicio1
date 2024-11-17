465015d691c9c016f5471737dee0cf9e3ddc99e4
cristian <cregueiromartinez@danielcastelao.org>
2024-11-17 13:04:46 +0100

    documentacion de la clase main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,23 @@ import entity.Persona;
 import java.math.BigDecimal;
 import java.util.List;
 
+/**
+ * Clase main donde ejecutamos el programa
+ * @author cristian
+ * @version 1.0
+ */
 public class Main {
 
     public static void main(String[] args) {
+        //Instanciamos el objeto de metodos
         Metodos metodos = new Metodos();
+        //hacemos una lista con el metodo que me devuelve una lista de objetos de tipo persona
         List<Persona> personas = metodos.getPersonas();
 
+        //instanciamos un objeto de la clase CrudHibernate
         CrudHibernate crud = new CrudHibernate();
 
+        //llamamos a los metodos que operan con la base de datos con hibernate
         crud.insertValuesPersonas(personas);
         crud.updateValuesPersonas(metodos);
         crud.deletePersona(BigDecimal.valueOf(3));
